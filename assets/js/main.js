0fb836afe8adf26029c7c5779ab7ea6371e2226c
Matheus Alves <matheusgalves.0604@gmail.com>
2025-02-25 21:56:32 -0300

    feat: enhance WordCounter functionality with form submission and result display

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,26 @@
 class WordCounter {
 
     constructor(){
-        this.button = document.getElementById("submit-button")
+        this.wordElement = document.getElementById("words")
+        this.form = document.getElementById("form-text")
         this.input = document.getElementById("input-text")
+        this.button = document.getElementById("submit-button")
+        this.table = document.getElementById("table-result")
+        this.table.style.visibility = "hidden"
+    }
+
+    showTable(){
+        
+        this.form.addEventListener("submit", (event) =>{
+            event.preventDefault();
+
+            let textValue = this.input.value
+            let words = this.counter(textValue)
+            let wordElement = this.wordElement
+
+            wordElement.textContent = words
+            this.table.style.visibility = "visible"
+        })
     }
 
     counter(text){
@@ -11,3 +29,7 @@ class WordCounter {
     }
 }
 
+
+
+wc = new WordCounter()
+wc.showTable()
